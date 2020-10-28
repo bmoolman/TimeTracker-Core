0e9bdb523d26739e246d3dc8430529f0d556fd87
Bruce Moolman <bruce.moolman@se.abb.com>
2020-10-28 15:01:35 +0100

    Added TTEvent Class Added Reference to the lib in the console.

--- TimeTrackerLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TimeTrackerLib
-{
-    public class Class1
-    {
-    }
-}
---
+++ TimeTrackerLib/TTEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTrackerLib
+{
+    public class TTEvent
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string StartUTC { get; set; }
+        public string EndUTC { get; set; }
+        public string Comments { get; set; }
+
+    }
+}
