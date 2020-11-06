703fae857e43dbf331a9b110c984ccc89d50694d
Bruce Moolman <bruce.moolman@se.abb.com>
2020-11-06 19:22:01 +0100

    Changed WPF type to PRISM application

--- TTWindowsWPF/App.xaml.cs
+++ TTWindowsWPF/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DryIoc;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
 using System.Windows;
 
 namespace TTWindowsWPF
@@ -11,7 +9,16 @@ namespace TTWindowsWPF
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Window CreateShell()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
